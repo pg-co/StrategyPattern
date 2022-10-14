1197a493df234b3683b211d0568fd483a22f3a7e
Marcel Meyer <marcel.m5@schule.bremen.de>
2022-10-14 12:09:56 +0200

    Init Projekt

---
+++ InsertionSort.cs
@@ -0,0 +1,21 @@
+using StrategyPattern.Interfaces;
+
+class InsertionSort : ISortStrategy {
+    public int[]? arr { get; set; }
+
+    public int[] sort(int[] arr) {
+       for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        int temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                  }
+            }
+            return arr;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 40, 10, 20, 30, 50 };
+var selectionSort = new InsertionSort();
+
+foreach(var item in selectionSort.sort(array)) {
+    Console.WriteLine(item.ToString());
+}
