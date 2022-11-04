442721ff9907f3df0b5fd6b7c3bc09422b92b385
Lennart Jürgens <lju@acadon.de>
2022-11-04 10:36:35 +0100

    implemented bubblesort

---
+++ interfaces/ISortStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.interfaces
+{
+    public interface ISortStrategy
+    {
+        public interface Strategy
+        {
+            public int[] Sort(int[] arr);
+        }
+    }
+}
