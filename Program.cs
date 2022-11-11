6e974b295ca8752c8b0c62fa7d7d16c7c842e75c
Marcel Meyer <marcel.m5@schule.bremen.de>
2022-11-11 11:20:04 +0100

    New Programm cs w/ Comparision

--- Program.cs
+++ Program.cs
@@ -1,7 +1,47 @@
-﻿int[] array = { -40, 10, 20, 30, 50 };
-var selectionSort = new BubbleSort();
-bool asc = true;
+﻿using StrategyPattern.Interfaces;
 
-foreach(var item in selectionSort.sort(array, asc)) {
-    Console.WriteLine(item.ToString());
+
+List<int> ausgang = new List<int>();
+Random rand = new Random();
+for (int i = 0 ; i < 20000 ; i++){
+    ausgang.Add(rand.Next());
+}
+
+var x = new NumberArray(ausgang.ToArray(),new Quicksort());
+var watch = System.Diagnostics.Stopwatch.StartNew();
+Console.WriteLine(x.sort(true));
+watch.Stop();
+Console.WriteLine("Quicksort TIME :" + watch.ElapsedMilliseconds);
+
+x.strat = new BubbleSort();
+watch.Restart();
+Console.WriteLine(x.sort(true));
+watch.Stop();
+Console.WriteLine("BUBBLE SORT TIME :" + watch.ElapsedMilliseconds);
+
+x.strat = new InsertionSort();
+watch.Restart();
+Console.WriteLine(x.sort(true));
+watch.Stop();
+Console.WriteLine("INSERTION SORT TIME :" + watch.ElapsedMilliseconds);
+
+x.strat = new SelectionSort();
+watch.Restart();
+Console.WriteLine(x.sort(true));
+watch.Stop();
+Console.WriteLine("SELECTION SORT TIME :" + watch.ElapsedMilliseconds);
+
+
+public class NumberArray{
+    int[] arr {get; set;}
+    public ISortStrategy strat {get; set;}
+
+    public NumberArray(int[] xArr, ISortStrategy xStrat){
+        this.arr = xArr;
+        this.strat = xStrat;
+    }
+    public int[] sort(bool xAsc){
+        return strat.sort(this.arr,xAsc);
+    }
+
 }
